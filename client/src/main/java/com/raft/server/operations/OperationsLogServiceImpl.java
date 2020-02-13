2002115dd62607e25783c85625d9fa369ff2d2c8
plshakoff@gmail.com <plshakoff@gmail.com>
2020-02-13 17:05:44 +0300

    Context controller 2

--- client/src/main/java/com/raft/server/operations/OperationsLogServiceImpl.java
+++ client/src/main/java/com/raft/server/operations/OperationsLogServiceImpl.java
@@ -1,14 +1,27 @@
 package com.raft.server.operations;
 
+import com.network.http.Http;
+import com.raft.server.exchange.ExchangeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 
 @Service
+@RequiredArgsConstructor
 public class OperationsLogServiceImpl implements OperationsLogService {
+
+    private final Http http;
+    private final ExchangeService exchangeService;
+
     @Override
     public List<Operation> all(Integer peerId) {
-        return null;
+        exchangeService.checkAvailable(peerId);
+        return http.callGet(peerId.toString(), new ParameterizedTypeReference<List<Operation>>() {}, "log").getBody();
     }
+
+
+
 }
